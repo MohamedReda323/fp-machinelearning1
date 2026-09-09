faf2824c45acc43d905a2f74c19a2e89c7d1114b
MohamedReda323 <mohamedgadalla436@gmail.com>
2026-09-09 15:08:51 +0300

    Add pandas import and load dataset from CSV

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 st.info("This is app builds a machine learning model!")
+df=pd.read_csv("https://raw.githubusercontent.com/MohamedReda323/fp-machinelearning1/refs/heads/master/loan_data.csv")
+df
