f9a10d98ff43adb29a6c22d8c1ecd15135de62e7
MohamedReda323 <mohamedgadalla436@gmail.com>
2026-09-09 17:18:12 +0300

    Enhance input features in Streamlit app
    Added sliders for loan amount, interest rate, loan to income ratio, credit history length, and credit score.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,7 @@ with st.expander("Data visualization"):
 
 with st. sidebar:
   st.header("Input feature")
-  #person_emp_exp,loan_amnt,loan_int_rate,loan_percent_income,
-  #cb_person_cred_hist_length,credit_score,loan_status
+  #credit_score,loan_status
   Gender = st.selectbox("Gender",("male","female"))
   Education = st.selectbox("Education",("High School", "Associate", "Bachelor", "Master", "Doctorate"))
   Home_ownership = st.selectbox("Home ownership",("RENT", "MORTGAGE", "OWN", "OTHER"))
@@ -33,5 +32,9 @@ with st. sidebar:
   person_age = st.slider("Age",20,78,26,1)
   person_income = st.slider("Income",8000.00,168667.12,67045.50)
   person_emp_exp = st.slider("Years of Experience",0,58,4,1)
-
+  loan_amnt = st.slider("Loan amount",500.00,23093.12,8000.00)
+  loan_int_rate = st.slider("Interest rate",5.42,19.59,11.01)
+  loan_percent_income = st.slider("Loan to income ratio",0.003,0.37,0.12)
+  cb_person_cred_hist_length = st.slider("Credit history length",2,15,4)
+  credit_score = st.slider("Credit score",497.50,773.50,640.00)
 
