ea64a3d81f362864621ad99ff45e54ddaa47a4f7
MohamedReda323 <mohamedgadalla436@gmail.com>
2026-09-10 00:07:08 +0300

    Fix input_df transformation to use values

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ scaler = joblib.load("scaler.pkl")
 numeric_col = ['person_age', 'person_income', 'person_emp_exp', 'loan_amnt',
        'loan_int_rate', 'loan_percent_income', 'cb_person_cred_hist_length',
        'credit_score']
-input_df = scaler.transform(input_df[numeric_col])
+input_df = scaler.transform(input_df[numeric_col].values)
 
 
 if hasattr(xgb_model, "feature_names_in_"):
