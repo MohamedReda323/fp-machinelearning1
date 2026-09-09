59eb175cb3f9a5d49498a24dc61357b84ffa8d08
MohamedReda323 <mohamedgadalla436@gmail.com>
2026-09-09 21:51:46 +0300

    Refactor input variables and add data collection
    Updated variable names for user inputs in the sidebar and added a data dictionary to collect input values.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,9 @@ with st.expander("Data visualization"):
 with st. sidebar:
   st.header("Input feature")
   #credit_score,loan_status
-  Gender = st.selectbox("Gender",("male","female"))
-  Education = st.selectbox("Education",("High School", "Associate", "Bachelor", "Master", "Doctorate"))
-  Home_ownership = st.selectbox("Home ownership",("RENT", "MORTGAGE", "OWN", "OTHER"))
+  person_gender = st.selectbox("Gender",("male","female"))
+  person_education = st.selectbox("Education",("High School", "Associate", "Bachelor", "Master", "Doctorate"))
+  person_home_ownership = st.selectbox("Home ownership",("RENT", "MORTGAGE", "OWN", "OTHER"))
   loan_intent = st.selectbox("Loan intent",("EDUCATION", "MEDICAL", "VENTURE", "PERSONAL", "DEBTCONSOLIDATION", "HOMEIMPROVEMENT"))
   previous_loan_defaults_on_file = st.selectbox("Loan defaults",("Yes","No"))
   person_age = st.slider("Age",20,78,26,1)
@@ -37,4 +37,27 @@ with st. sidebar:
   loan_percent_income = st.slider("Loan to income ratio",0.003,0.37,0.12)
   cb_person_cred_hist_length = st.slider("Credit history length",2,15,4)
   credit_score = st.slider("Credit score",497.50,773.50,640.00)
+
+  data = {
+      "person_age":person_age,
+      "person_gender":person_gender,
+      "person_education":person_education,
+      "person_income":person_income,
+      "person_emp_exp":person_emp_exp,
+      "person_home_ownership":person_home_ownership,
+      "loan_amnt":loan_amnt,
+      "loan_intent":loan_intent,
+      "loan_int_rate":loan_int_rate,
+      "loan_percent_income":loan_percent_income,
+      "cb_person_cred_hist_length":cb_person_cred_hist_length,
+      "credit_score":credit_score,
+      "previous_loan_defaults_on_file":previous_loan_defaults_on_file
+  }
+  input_df = pd.DataFrame(data,index[0])
+  input_df
+
+
+
+
+
 
