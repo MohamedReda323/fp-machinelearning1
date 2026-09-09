620778ef890e2033ed12d3ce02aabfa916882122
MohamedReda323 <mohamedgadalla436@gmail.com>
2026-09-09 16:37:11 +0300

    Refactor sidebar and add input features
    Updated sidebar layout and added input features for user selection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,15 @@ with st.expander("Data"):
 with st.expander("Data visualization"):
   st.scatter_chart(data=df, x="person_age", y="person_emp_exp", color="loan_status")
 
-with st.sidebar:
+with st. sidebar:
   st.header("Input feature")
+  #person_age,person_income,person_emp_exp,loan_amnt,loan_int_rate,loan_percent_income,
+  #cb_person_cred_hist_length,credit_score,loan_status
+  Gender = st.selectbox("Gender",("male","female"))
+  Education = st.selectbox("Education",("High School", "Associate", "Bachelor", "Master", "Doctorate"))
+  Home_ownership = st.selectbox("Home ownership",("RENT", "MORTGAGE", "OWN", "OTHER"))
+  loan_intent = st.selectbox("Loan intent",("EDUCATION", "MEDICAL", "VENTURE", "PERSONAL", "DEBTCONSOLIDATION", "HOMEIMPROVEMENT"))
+  previous_loan_defaults_on_file = st.selectbox("Loan defaults",("Yes","No"))
+  person_age = st.slider("Age",20.00,78.00,26.00)
+
+
