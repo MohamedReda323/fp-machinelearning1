96429e5653a15346a867a7aa21bf98b68a470848
MohamedReda323 <mohamedgadalla436@gmail.com>
2026-09-09 23:08:45 +0300

    Fix formatting issue in loan intent conditions

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ with st. sidebar:
       "loan_intent_HOMEIMPROVEMENT": [1 if loan_intent == "HOMEIMPROVEMENT" else 0],
       "loan_intent_MEDICAL": [1 if loan_intent == "MEDICAL" else 0],
       "loan_intent_PERSONAL": [1 if loan_intent == "PERSONAL" else 0],
-      "loan_intent_VENTURE": [1 if loan_intent == "VENTURE" else 0],
+      "loan_intent_VENTURE": [1 if loan_intent == "VENTURE" else 0]
 
 
       # "person_home_ownership_MORTGAGE": [1 if person_home_ownership == "MORTGAGE" else 0],
