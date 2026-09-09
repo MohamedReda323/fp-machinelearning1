2c818d48e4e30fa5f6e0b536da244dc3d103d000
MohamedReda323 <mohamedgadalla436@gmail.com>
2026-09-10 00:10:49 +0300

    Refactor scaling of numeric columns in input_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,9 @@ scaler = joblib.load("scaler.pkl")
 numeric_col = ['person_age', 'person_income', 'person_emp_exp', 'loan_amnt',
        'loan_int_rate', 'loan_percent_income', 'cb_person_cred_hist_length',
        'credit_score']
-input_df[numeric_col] = scaler.transform(input_df[numeric_col].values)
+
+for i, col in enumerate(numeric_col):
+    input_df[col] = scaler.transform(input_df[[col]].values) 
 
 
 if hasattr(xgb_model, "feature_names_in_"):
